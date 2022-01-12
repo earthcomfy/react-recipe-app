0c98cfe59057f67751ba820cce60628d661281d7
Hannah <66206865+earthcomfy@users.noreply.github.com>
2022-01-12 19:01:42 +0300

    Remove backend folder

--- backend/recipe/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import RecipeCategory, Recipe, RecipeLike
-
-# Register your models here.
-admin.site.register(RecipeCategory)
-admin.site.register(Recipe)
-admin.site.register(RecipeLike)
